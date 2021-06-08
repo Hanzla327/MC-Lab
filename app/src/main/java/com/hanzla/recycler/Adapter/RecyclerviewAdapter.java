85774a177b4fad4b2bdb8d0081fd743b5ce2ff6b
Hanzla <hanzlasaluu2000@gmail.com>
2021-06-08 16:12:32 -0700

    commit  1

---
+++ app/src/main/java/com/hanzla/recycler/Adapter/RecyclerviewAdapter.java
@@ -0,0 +1,64 @@
+package com.hanzla.recycler.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.hanzla.recycler.Model.Student;
+import com.hanzla.recycler.R;
+
+import java.util.List;
+
+public class RecyclerviewAdapter extends RecyclerView.Adapter<RecyclerviewAdapter.ViewHolder> {
+   private Context context;
+   private List<Student> studentList;
+
+    public RecyclerviewAdapter() {
+    }
+
+    public RecyclerviewAdapter(Context context, List<Student> studentList) {
+        this.context = context;
+        this.studentList = studentList;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerviewAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.cardd,parent,false);
+
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerviewAdapter.ViewHolder holder, int position) {
+    Student student =studentList.get(position);
+    holder.txtName.setText(student.getName());
+    holder.txtRollNo.setText(student.getRollNo());
+    }
+
+    @Override
+    public int getItemCount() {
+        return studentList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        TextView txtName,txtRollNo;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            txtName = itemView.findViewById(R.id.txtName);
+            txtRollNo =itemView.findViewById(R.id.txtRollNo);
+
+        }
+
+        @Override
+        public void onClick(View v) {
+
+        }
+    }
+}
---
+++ app/src/main/java/com/hanzla/recycler/MainActivity.java
@@ -0,0 +1,34 @@
+package com.hanzla.recycler;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import com.hanzla.recycler.Adapter.RecyclerviewAdapter;
+import com.hanzla.recycler.Model.Student;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    List<Student> studentList =new ArrayList<>();
+    RecyclerView recyclerView;
+    RecyclerviewAdapter recyclerviewAdapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        studentList.add(new Student("1","Electrician"));
+        studentList.add(new Student("2","Mechanic"));
+        studentList.add(new Student("3","Plumber"));
+        studentList.add(new Student("4","Carpenter"));
+        recyclerviewAdapter=new RecyclerviewAdapter(MainActivity.this,studentList);
+        recyclerView=findViewById(R.id.recyclerview);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(recyclerviewAdapter);
+    }
+}
